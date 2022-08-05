c36c6d98de77a2dff94c1c588e5a766904fbc03a
Abdul-bakri <olayidebakri@gmail.com>
2022-08-05 09:01:56 +0100

    vpc-eks

---
+++ security_group.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "cluster" {
+  name        = "terraform-eks-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-demo"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  default = "example"
+  type    = string
+}
+
+
+
+variable "aws_region" {
+  type = string
+  default     = "us-east-1"
+}
